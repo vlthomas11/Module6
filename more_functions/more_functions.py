f81f79701771d6134387693de267f75c2f427d76
VLThomas11 <vthomas2@DMACC.edu>
2020-02-24 09:15:27 -0600

    update

--- more_functions/more_functions.py
+++ more_functions/more_functions.py
@@ -1,2 +1,3 @@
-def multiply_string():
-    pass
+def multiply_string(name,num):
+    multstring = name * num
+    return multstring
--- test_functions/test_string_functions.py
+++ test_functions/test_string_functions.py
@@ -1,6 +1,11 @@
 import unittest
 from more_functions import more_functions as topic3
 
+
 class MyTestClass(unittest.TestCase):
     def test_multiple_string(self):
-        self.assertEqual('vickileevickileevickilee')
+        self.assertEqual(topic3.multiply_string('vickilee', 3), 'vickileevickileevickilee')
+    def test_multiply_string_fail(self):
+        self.assertIsNot(topic3.multiply_string('vickilee',5),'vickileevickilee')
+    def test_multiply_string2(self):
+        self.assertEqual(topic3.multiply_string('vickilee',4),'vickilee'*4)
