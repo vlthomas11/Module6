e4eae677db475cf71a2114a59e57375d6d93f9fe
VLThomas11 <vthomas2@DMACC.edu>
2020-02-24 09:17:28 -0600

    update

--- more_functions/more_functions.py
+++ more_functions/more_functions.py
@@ -1,3 +1,4 @@
 def multiply_string(name,num):
+    """This function takes parameters, name and num. It multiplies the string and the number and returns the value"""
     multstring = name * num
     return multstring
