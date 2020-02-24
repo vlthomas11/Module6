07e24d5efefd6dc3a21f8abc92fffba1394bd0cc
VLThomas11 <vthomas2@DMACC.edu>
2020-02-24 09:37:49 -0600

    update

---
+++ more_functions/validate_input_in_fucntions.py
@@ -0,0 +1,2 @@
+def score_input(test_name,test_score = 0,invalid_message = 'Invalid test score, try again'):
+    pass
---
+++ test_functions/test_valid_input_functions.py
@@ -0,0 +1,6 @@
+import unittest
+from more_functions import validate_input_in_fucntions as topic4
+
+class MyTestClass(unittest.TestCase):
+    def test_score_input_test_score_valid(self):
+        self.assertEqual()
