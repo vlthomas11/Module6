c525e8394efdd8131c4b555b30645da4a4058f37
VLThomas11 <vthomas2@DMACC.edu>
2020-02-24 12:58:08 -0600

    update

--- test_functions/test_valid_input_functions.py
+++ test_functions/test_valid_input_functions.py
@@ -4,7 +4,7 @@ from more_functions import validate_input_in_fucntions as topic4
 
 class MyTestClass(unittest.TestCase):
     def test_score_input_test_score_valid(self):
-        self.assertEqual(topic4.score_input('vickilee', 10), 'vickilee : 10')
+        self.assertEqual(topic4.score_input('vickilee', ), 'vickilee : 0')
 
     def test_score_input_test_score_below_range(self):
         self.assertEqual(topic4.score_input('vickilee',110),'Invalid test score, try again')
