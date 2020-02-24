c7c81cec7f33bff12c4897068b7697afed8f9d0c
VLThomas11 <vthomas2@DMACC.edu>
2020-02-24 10:50:19 -0600

    update

--- more_functions/validate_input_in_fucntions.py
+++ more_functions/validate_input_in_fucntions.py
@@ -1,2 +1,9 @@
-def score_input(test_name,test_score = 0,invalid_message = 'Invalid test score, try again'):
-    pass
+def score_input(test_name, test_score=0, invalid_message='Invalid test score, try again'):
+    """Takes parameters"""
+    while True:
+        if test_score >= 0:
+            return test_name + " : " + str(test_score)
+        else:
+            print(invalid_message)
+            return False
+
