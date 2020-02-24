9301b2075f38695b6d480d238632bf7e81823762
VLThomas11 <vthomas2@DMACC.edu>
2020-02-24 10:55:21 -0600

    update

--- more_functions/validate_input_in_fucntions.py
+++ more_functions/validate_input_in_fucntions.py
@@ -1,9 +1,8 @@
 def score_input(test_name, test_score=0, invalid_message='Invalid test score, try again'):
     """Takes parameters"""
     while True:
-        if test_score >= 0:
+        if test_score >= 0 and test_score < 100:
             return test_name + " : " + str(test_score)
         else:
-            print(invalid_message)
-            return False
+            return invalid_message
 
--- test_functions/test_valid_input_functions.py
+++ test_functions/test_valid_input_functions.py
@@ -1,15 +1,19 @@
 import unittest
 from more_functions import validate_input_in_fucntions as topic4
 
+
 class MyTestClass(unittest.TestCase):
     def test_score_input_test_score_valid(self):
-        self.assertEqual(topic4.score_input('vickilee',10),'vickilee : 10')
+        self.assertEqual(topic4.score_input('vickilee', 10), 'vickilee : 10')
+
     def test_score_input_test_score_below_range(self):
         self.assertEqual()
-    # def test_score_input_test_score_above_range(self):
-    #     self.assertEqual()
+
+    def test_score_input_test_score_above_range(self):
+        self.assertEqual(topic4.score_input('vickilee', 110), 'Invalid test score, try again')
     # def test_test_score_non_numeric(self):
     #     self.assertEqual()
+
 
 if __name__ == '__main__':
     unittest.main()
